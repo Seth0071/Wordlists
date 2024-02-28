f0c0503877481d89d7feb1b816ffbdc42917303b
Seth007 <150815705+Seth0071@users.noreply.github.com>
2024-02-28 19:27:59 +0100

    Update 4-digit_code.py

--- Number Wordlists /4-digit_code.py
+++ Number Wordlists /4-digit_code.py
@@ -9,17 +9,17 @@ for i in range(9999):
     if (password <= 9):
         passw = "000" + str(password)
         #list1.write(f"{passw}\n")  #    < Delete comment for wordlist file
-        print(passw) #     < Delete comment for shell output of wordlist
+        #print(passw) #     < Delete comment for shell output of wordlist
     elif (password <= 99):
         passw = "00" + str(password)
         #list1.write(f"{passw}\n") #    < Delete comment for wordlist file
-        print(passw) #     < Delete comment for shell output of wordlist
+        #print(passw) #     < Delete comment for shell output of wordlist
     elif (password <= 999):
         passw = "0" + str(password)
         #list1.write(f"{passw}\n") #    < Delete comment for wordlist file
-        print(passw) #     < Delete comment for shell output of wordlist
+        #print(passw) #     < Delete comment for shell output of wordlist
     elif (password <= 9999):
         passw = "" + str(password)
         #list1.write(f"{passw}\n") #    < Delete comment for wordlist file
-        print(passw) #     < Delete comment for shell output of wordlist
+        #print(passw) #     < Delete comment for shell output of wordlist
 list1.close()
